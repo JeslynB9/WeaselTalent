bc42a9baaae0849a678813902bbfd40c133baf4d
Takaya <takayaueno0712@gmail.com>
2025-12-13 18:20:48 +1100

    Modified users.py and main.py for the API logic for users

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,24 @@
 # entry point
 from db import engine
 from models import Base
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from users import router as users_router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # create all tables defined on `Base`
 Base.metadata.create_all(bind=engine)
+
+# register user routes
+app.include_router(users_router)
+
+# to start server: uvicorn main:app --reload
