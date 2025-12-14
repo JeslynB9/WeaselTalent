1851701d19b0cd8b4caca031bf97cad154ff069f
destinee-horvath <dhor0241@uni.sydney.edu.au>
2025-12-14 17:53:37 +1100

    cleaned main.py, added seeding

--- backend/db.py
+++ backend/db.py
@@ -4,10 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # use URL from .env if exists; else 
 DATABASE_URL = os.getenv (
     "DATABASE_URL",
-    "sqlite:///./app.db"
+    # "sqlite:///./app.db", 
+    f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}"
 )
 
 # the actual db connection
